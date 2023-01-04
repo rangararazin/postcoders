eeb8dd6de6a3e828d03da591418390a70c8b88bf
rangararazin <rangararazin@outlook.com>
2023-01-04 19:53:35 +0000

    this commit solve task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useEffect, useState } from 'react'
-import { getAreaData } from './api'
+import { useEffect, useState } from "react";
+import { getAreaData } from "./api";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   const [areas, setAreas] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const load = async () => {
     try {
-      const areaData = await getAreaData()
+      const areaData = await getAreaData();
 
       areas.concat(areaData);
-  
+
       setAreas(areas);
     } catch (error) {
-      window.alert("todo: fix app")
+      window.alert("todo: fix app");
     }
-  }
+  };
 
   useEffect(() => {
+    getAreaData().then((res) => {
+      setAreas(res);
+      setLoading(false);
+    });
     load();
   }, []);
 
-  return (
+  return loading ? (
+    <p>loading....</p>
+  ) : (
     <div className="App">
       <h1>Postcoders</h1>
       <h2>{`Areas for BB10: ${areas.length}`}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
